e723cc3db5bb6b36915127890d89d4a1e049bb36
Jennifer Passaro <jennifer.wheatley@gmail.com>
2015-02-01 21:17:03 -0500

    HTML updates to "me" page
    Added grid system and containers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'application#index'
+  get "/me" => 'application#me'
   get '/tables' => 'application#tables'
   get '/forms' => 'application#forms'
 end
